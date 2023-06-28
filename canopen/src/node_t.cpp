4f5b5772f31f48bbe07bb3280dc5b42aab4c984c
jaw <wenzel@neobotix.de>
2023-06-28 08:16:16 +0200

    added canopen server functions

--- canopen/src/node_t.cpp
+++ canopen/src/node_t.cpp
@@ -36,6 +36,49 @@ static uint8_t convert_nmt_command(const nmt_command_e &command){
 }
 
 
+// (error class, error code, additional code)
+static std::tuple<uint8_t, uint8_t, uint16_t> convert_sdo_error(const sdo_error_e &error){
+	switch(error){
+	case sdo_error_e::NO_ERROR: throw std::logic_error("No error");
+	case sdo_error_e::TOGGLE_BIT_ERROR: return std::make_tuple(5, 3, 0);
+	case sdo_error_e::TIMEOUT: return std::make_tuple(5, 4, 0);
+	case sdo_error_e::INVALID_COMMAND: return std::make_tuple(5, 4, 1);
+	case sdo_error_e::INVALID_BLOCK_SIZE: return std::make_tuple(5, 4, 2);
+	case sdo_error_e::INVALID_BLOCK_SEQUENCE_NUMBER: return std::make_tuple(5, 4, 3);
+	case sdo_error_e::INVALID_BLOCK_CRC: return std::make_tuple(5, 4, 4);
+	case sdo_error_e::OUT_OF_MEMORY: return std::make_tuple(5, 4, 5);
+	case sdo_error_e::OBJECT_ACCESS_WRITE_ONLY: return std::make_tuple(6, 1, 1);
+	case sdo_error_e::OBJECT_ACCESS_READ_ONLY: return std::make_tuple(6, 1, 2);
+	case sdo_error_e::OBJECT_NON_EXISTENT: return std::make_tuple(6, 2, 0);
+	case sdo_error_e::INDEX_RESERVED: return std::make_tuple(6, 4, 0);
+	case sdo_error_e::CANNOT_MAP_TO_PDO: return std::make_tuple(6, 4, 0x41);
+	case sdo_error_e::PDO_LENGTH_EXCEEDED: return std::make_tuple(6, 4, 0x42);
+	case sdo_error_e::GENERAL_PARAMETER_INCOMPATIBILITY: return std::make_tuple(6, 4, 0x43);
+	case sdo_error_e::GENERAL_INTERNAL_INCOMPATIBILITY: return std::make_tuple(6, 4, 0x44);        // or is it 0x47? Sources differ.
+	case sdo_error_e::HARDWARE_FAULT: return std::make_tuple(6, 6, 0);
+	case sdo_error_e::TYPE_CONFLICT: return std::make_tuple(6, 7, 0);
+	case sdo_error_e::TYPE_MISMATCH: return std::make_tuple(6, 7, 0x10);
+	case sdo_error_e::OBJECT_LENGTH_TOO_HIGH: return std::make_tuple(6, 7, 0x12);
+	case sdo_error_e::OBJECT_LENGTH_TOO_LOW: return std::make_tuple(6, 7, 0x13);
+	case sdo_error_e::OBJECT_ATTRIBUTE_INCONSISTENT: return std::make_tuple(6, 9, 0);
+	case sdo_error_e::SUB_INDEX_DOES_NOT_EXIST: return std::make_tuple(6, 9, 0x11);
+	case sdo_error_e::PARAMETER_VALUE_OUT_OF_RANGE: return std::make_tuple(6, 9, 0x30);
+	case sdo_error_e::PARAMETER_VALUE_TOO_HIGH: return std::make_tuple(6, 9, 0x31);
+	case sdo_error_e::PARAMETER_VALUE_TOO_LOW: return std::make_tuple(6, 9, 0x32);
+	case sdo_error_e::SUB_PARAMETER_VALUE_OUT_OF_RANGE: return std::make_tuple(6, 9, 0x33);
+	case sdo_error_e::MAX_VALUE_LESS_THAN_MIN_VALUE: return std::make_tuple(6, 9, 0x36);
+	case sdo_error_e::CONNECTION_ERROR: return std::make_tuple(6, 10, 0x23);
+	case sdo_error_e::DATA_TRANSFER: return std::make_tuple(8, 0, 0x20);
+	case sdo_error_e::DATA_TRANSFER_LOCAL: return std::make_tuple(8, 0, 0x21);
+	case sdo_error_e::DATA_TRANSFER_DEVICE_STATE: return std::make_tuple(8, 0, 0x22);
+	case sdo_error_e::OBJECT_DICTIONARY_FAIL: return std::make_tuple(8, 0, 0x23);
+	case sdo_error_e::NO_DATA: return std::make_tuple(8, 0, 0x24);
+	case sdo_error_e::GENERAL_ERROR: return std::make_tuple(8, 0, 0);
+	}
+	throw std::logic_error("Unknown or unspecific SDO error code: " + vnx::to_string_value(error));
+}
+
+
 static void set_ccs(CAN_Frame &frame, const sdo_ccs_e &ccs){
 	uint8_t ccs_byte;
 	switch(ccs){
@@ -52,6 +95,153 @@ static void set_ccs(CAN_Frame &frame, const sdo_ccs_e &ccs){
 	const uint8_t zero_mask = 0b00011111;
 	frame.data[0] &= zero_mask;
 	frame.data[0] |= (ccs_byte << 5);
+}
+
+
+static void set_scs(CAN_Frame &frame, const sdo_scs_e &scs){
+	uint8_t scs_byte;
+	switch(scs){
+	case sdo_scs_e::INIT_DOWNLOAD_RESPONSE: scs_byte = 3; break;
+	case sdo_scs_e::INIT_UPLOAD_RESPONSE: scs_byte = 2; break;
+	case sdo_scs_e::SEGMENT_DOWNLOAD_RESPONSE: scs_byte = 1; break;
+	case sdo_scs_e::SEGMENT_UPLOAD_RESPONSE: scs_byte = 0; break;
+	case sdo_scs_e::BLOCK_DOWNLOAD: scs_byte = 6; break;
+	case sdo_scs_e::BLOCK_UPLOAD: scs_byte = 5; break;
+	case sdo_scs_e::ABORT: scs_byte = 4; break;
+	default: throw std::logic_error("Unknown SCS " + vnx::to_string_value(scs));
+	}
+
+	const uint8_t zero_mask = 0b00011111;
+	frame.data[0] &= zero_mask;
+	frame.data[0] |= (scs_byte << 5);
+}
+
+
+static std::shared_ptr<CAN_Frame> transfer_expedited(uint16_t index, uint8_t subindex, uint32_t data, uint32_t num_bytes){
+	if(num_bytes > 4){
+		throw std::logic_error("Cannot transfer more than 4 bytes in expedited transfer");
+	}
+	const uint8_t no_data = (4 - num_bytes) << 2;
+	const uint8_t expedited = 1 << 1;
+	const uint8_t size_indicator = 1;
+
+	auto frame = CAN_Frame::create();
+	// Remember to set id and ccs/scs
+	frame->size = 8;
+	frame->data[0] |= no_data | expedited | size_indicator;
+	frame->data[1] = index;
+	frame->data[2] = (index >> 8);
+	frame->data[3] = subindex;
+	frame->data[4] = data;
+	frame->data[5] = (data >> 8);
+	frame->data[6] = (data >> 16);
+	frame->data[7] = (data >> 24);
+	return frame;
+}
+
+
+static std::vector<std::shared_ptr<CAN_Frame>> transfer_segmented(uint16_t index, uint8_t subindex, const std::vector<uint8_t> &data){
+	std::vector<std::shared_ptr<CAN_Frame>> frames;
+
+	{
+		// first frame initiates the transfer
+		const uint8_t size_indicator = 1;
+		const uint32_t data_size = data.size();
+
+		auto frame = CAN_Frame::create();
+		// Remember to set id and ccs/scs
+		frame->size = 8;
+		frame->data[0] |= size_indicator;
+		frame->data[1] = index;
+		frame->data[2] = (index >> 8);
+		frame->data[3] = subindex;
+		frame->data[4] = data_size;
+		frame->data[5] = (data_size >> 8);
+		frame->data[6] = (data_size >> 16);
+		frame->data[7] = (data_size >> 24);
+		frames.push_back(frame);
+	}
+
+	// remaining frames continue the transfer
+	size_t data_offset = 0;
+	bool toggle_bit = false;
+	while(data_offset < data.size()){
+		const size_t max_bytes = 7;
+		const size_t num_bytes = std::min(data.size() - data_offset, max_bytes);
+		const uint8_t no_data = max_bytes - num_bytes;
+		const bool is_last = (data_offset + num_bytes >= data.size());
+
+		auto frame = CAN_Frame::create();
+		// Remember to set id and ccs/scs
+		frame->size = 8;
+		frame->data[0] |= (toggle_bit << 4) | (no_data << 1) | is_last;
+		for(size_t i=0; i<num_bytes; i++){
+			frame->data[1+i] = data[data_offset+i];
+		}
+		frames.push_back(frame);
+
+		data_offset += num_bytes;
+		toggle_bit = !toggle_bit;
+	}
+
+	return frames;
+}
+
+
+static std::pair<std::vector<uint8_t>, bool> get_transferred_data_first_frame(const CAN_Frame &frame){
+	const uint8_t no_data = (frame.data[0] >> 2) & 0b11;
+	const bool is_expedited = (frame.data[0] >> 1) & 0b1;
+	const bool size_indicator = frame.data[0] & 0b1;
+	if(is_expedited){
+		if(!size_indicator){
+			throw std::runtime_error("Expedited transfer with no data size given");
+		}
+		const size_t num_bytes = 4 - no_data;
+		std::vector<uint8_t> data;
+		for(size_t i=0; i<num_bytes; i++){
+			data.push_back(frame.data[4+i]);
+		}
+		return std::make_pair(data, true);
+	}else{
+		if(!size_indicator){
+			throw std::runtime_error("Segmented transfer init with no data size given");
+		}
+		//const uint32_t data_length = frame.data[4] | (frame.data[5] << 8) | (frame.data[6] << 16) | (frame.data[7] << 24);
+		std::vector<uint8_t> data;
+		for(size_t i=0; i<4; i++){
+			data.push_back(frame.data[4+i]);
+		}
+		return std::make_pair(data, false);
+	}
+}
+
+
+static std::pair<std::vector<uint8_t>, bool> get_transferred_data_next_frame(const CAN_Frame &frame){
+	//const bool toggle_bit = (frame.data[0] >> 4) & 0b1;
+	const uint8_t no_data = (frame.data[0] >> 1) & 0b111;
+	const bool is_last = frame.data[0] & 0b1;
+	const size_t num_bytes = 7 - no_data;
+	std::vector<uint8_t> data;
+	for(size_t i=0; i<num_bytes; i++){
+		data.push_back(frame.data[1+i]);
+	}
+	return std::make_pair(data, is_last);
+}
+
+
+static std::shared_ptr<CAN_Frame> abort_transfer(uint16_t index, uint8_t subindex, const sdo_error_e &error){
+	const auto codes = convert_sdo_error(error);
+	auto frame = CAN_Frame::create();
+	// Remember to set id and ccs/scs
+	frame->size = 8;
+	frame->data[1] = index;
+	frame->data[2] = (index >> 8);
+	frame->data[3] = subindex;
+	frame->data[4] = std::get<2>(codes);
+	frame->data[5] = (std::get<2>(codes) >> 8);
+	frame->data[6] = std::get<1>(codes);
+	frame->data[7] = std::get<0>(codes);
+	return frame;
 }
 
 
@@ -315,76 +505,56 @@ sdo_error_e node_t::get_sdo_error(const CAN_Frame &frame) const{
 
 
 std::shared_ptr<const CAN_Frame> node_t::download_expedited(const uint16_t &index, const uint8_t &subindex, const uint32_t &data, const uint32_t &num_bytes) const{
-	if(num_bytes > 4){
-		throw std::logic_error("Cannot transfer more than 4 bytes in expedited transfer");
-	}
-	const uint8_t no_data = (4 - num_bytes) << 2;
-	const uint8_t expedited = 1 << 1;
-	const uint8_t size_indicator = 1;
-
-	auto frame = CAN_Frame::create();
+	auto frame = transfer_expedited(index, subindex, data, num_bytes);
 	frame->id = rx_sdo;
-	frame->size = 8;
 	set_ccs(*frame, sdo_ccs_e::INIT_DOWNLOAD);
-	frame->data[0] |= no_data | expedited | size_indicator;
-	frame->data[1] = index;
-	frame->data[2] = (index >> 8);
-	frame->data[3] = subindex;
-	frame->data[4] = data;
-	frame->data[5] = (data >> 8);
-	frame->data[6] = (data >> 16);
-	frame->data[7] = (data >> 24);
+	return frame;
+}
+
+
+std::shared_ptr<const CAN_Frame> node_t::upload_expedited(const uint16_t &index, const uint8_t &subindex, const uint32_t &data, const uint32_t &num_bytes) const{
+	auto frame = transfer_expedited(index, subindex, data, num_bytes);
+	frame->id = tx_sdo;
+	set_scs(*frame, sdo_scs_e::INIT_UPLOAD_RESPONSE);
 	return frame;
 }
 
 
 std::vector<std::shared_ptr<const CAN_Frame>> node_t::download_segmented(const uint16_t &index, const uint8_t &subindex, const std::vector<uint8_t> &data) const{
-	std::vector<std::shared_ptr<const CAN_Frame>> frames;
+	auto frames = transfer_segmented(index, subindex, data);
 
-	{
-		// first frame initiates the download
-		const uint8_t size_indicator = 1;
-		const uint32_t data_size = data.size();
-
-		auto frame = CAN_Frame::create();
+	std::vector<std::shared_ptr<const CAN_Frame>> result;
+	bool after_first = false;
+	for(auto frame : frames){
 		frame->id = rx_sdo;
-		frame->size = 8;
-		set_ccs(*frame, sdo_ccs_e::INIT_DOWNLOAD);
-		frame->data[0] |= size_indicator;
-		frame->data[1] = index;
-		frame->data[2] = (index >> 8);
-		frame->data[3] = subindex;
-		frame->data[4] = data_size;
-		frame->data[5] = (data_size >> 8);
-		frame->data[6] = (data_size >> 16);
-		frame->data[7] = (data_size >> 24);
-		frames.push_back(frame);
-	}
-
-	// remaining frames just continue the download
-	size_t data_offset = 0;
-	bool toggle_bit = false;
-	while(data_offset < data.size()){
-		const size_t max_bytes = 7;
-		const size_t num_bytes = std::min(data.size() - data_offset, max_bytes);
-		const uint8_t no_data = max_bytes - num_bytes;
-		const bool is_last = (data_offset + num_bytes >= data.size());
-
-		auto frame = CAN_Frame::create();
-		frame->id = rx_sdo;
-		frame->size = 8;
-		set_ccs(*frame, sdo_ccs_e::SEGMENT_DOWNLOAD);
-		frame->data[0] |= (toggle_bit << 4) | (no_data << 1) | is_last;
-		for(size_t i=0; i<num_bytes; i++){
-			frame->data[1+i] = data[data_offset+i];
+		if(after_first){
+			set_ccs(*frame, sdo_ccs_e::SEGMENT_DOWNLOAD);
+		}else{
+			set_ccs(*frame, sdo_ccs_e::INIT_DOWNLOAD);
 		}
-		frames.push_back(frame);
-
-		data_offset += num_bytes;
-		toggle_bit = !toggle_bit;
+		after_first = true;
+		result.push_back(frame);
 	}
+	return result;
+}
 
-	return frames;
+
+std::vector<std::shared_ptr<const CAN_Frame>> node_t::upload_segmented(const uint16_t &index, const uint8_t &subindex, const std::vector<uint8_t> &data) const{
+	auto frames = transfer_segmented(index, subindex, data);
+
+	std::vector<std::shared_ptr<const CAN_Frame>> result;
+	bool after_first = false;
+	for(auto frame : frames){
+		frame->id = tx_sdo;
+		if(after_first){
+			set_scs(*frame, sdo_scs_e::SEGMENT_UPLOAD_RESPONSE);
+		}else{
+			set_scs(*frame, sdo_scs_e::INIT_UPLOAD_RESPONSE);
+		}
+		after_first = true;
+		result.push_back(frame);
+	}
+	return result;
 }
 
 
@@ -393,6 +563,18 @@ std::shared_ptr<const CAN_Frame> node_t::upload_request(const uint16_t &index, c
 	frame->id = rx_sdo;
 	frame->size = 8;
 	set_ccs(*frame, sdo_ccs_e::INIT_UPLOAD);
+	frame->data[1] = index;
+	frame->data[2] = (index >> 8);
+	frame->data[3] = subindex;
+	return frame;
+}
+
+
+std::shared_ptr<const CAN_Frame> node_t::download_confirm(const uint16_t &index, const uint8_t &subindex) const{
+	auto frame = CAN_Frame::create();
+	frame->id = tx_sdo;
+	frame->size = 8;
+	set_scs(*frame, sdo_scs_e::INIT_DOWNLOAD_RESPONSE);
 	frame->data[1] = index;
 	frame->data[2] = (index >> 8);
 	frame->data[3] = subindex;
@@ -416,49 +598,69 @@ std::pair<std::shared_ptr<const CAN_Frame>, std::shared_ptr<const CAN_Frame>> no
 }
 
 
+std::pair<std::shared_ptr<const CAN_Frame>, std::shared_ptr<const CAN_Frame>> node_t::download_segment_request(const uint16_t &index, const uint8_t &subindex) const{
+	auto frame_1 = CAN_Frame::create();
+	frame_1->id = tx_sdo;
+	frame_1->size = 8;
+	set_scs(*frame_1, sdo_scs_e::SEGMENT_DOWNLOAD_RESPONSE);
+	frame_1->data[1] = index;
+	frame_1->data[2] = (index >> 8);
+	frame_1->data[3] = subindex;
+
+	auto frame_2 = vnx::clone(frame_1);
+	frame_2->data[0] |= (1 << 4);
+
+	return std::make_pair(frame_1, frame_2);
+}
+
+
 std::pair<std::vector<uint8_t>, bool> node_t::get_uploaded_data(const CAN_Frame &frame) const{
 	const auto scs = get_sdo_scs(frame);
 	if(scs != sdo_scs_e::INIT_UPLOAD_RESPONSE && scs != sdo_scs_e::SEGMENT_UPLOAD_RESPONSE){
 		throw std::logic_error("Frame is not an upload response: " + vnx::to_string_value(scs));
 	}
 
-	std::vector<uint8_t> data;
 	if(scs == sdo_scs_e::INIT_UPLOAD_RESPONSE){
-		const uint8_t no_data = (frame.data[0] >> 2) & 0b11;
-		const bool is_expedited = (frame.data[0] >> 1) & 0b1;
-		const bool size_indicator = frame.data[0] & 0b1;
-		if(is_expedited){
-			if(!size_indicator){
-				throw std::runtime_error("Expedited transfer with no data size given");
-			}
-			const size_t num_bytes = 4 - no_data;
-			for(size_t i=0; i<num_bytes; i++){
-				data.push_back(frame.data[4+i]);
-			}
-			return std::make_pair(data, true);
-		}else{
-			if(!size_indicator){
-				throw std::runtime_error("Segmented transfer init with no data size given");
-			}
-			//const uint32_t data_length = frame.data[4] | (frame.data[5] << 8) | (frame.data[6] << 16) | (frame.data[7] << 24);
-			for(size_t i=0; i<4; i++){
-				data.push_back(frame.data[4+i]);
-			}
-			return std::make_pair(data, false);
-		}
+		return get_transferred_data_first_frame(frame);
 	}else if(scs == sdo_scs_e::SEGMENT_UPLOAD_RESPONSE){
-		//const bool toggle_bit = (frame.data[0] >> 4) & 0b1;
-		const uint8_t no_data = (frame.data[0] >> 1) & 0b111;
-		const bool is_last = frame.data[0] & 0b1;
-		const size_t num_bytes = 7 - no_data;
-		for(size_t i=0; i<num_bytes; i++){
-			data.push_back(frame.data[1+i]);
-		}
-		return std::make_pair(data, is_last);
+		return get_transferred_data_next_frame(frame);
 	}
 
 	// -Wreturn-type
 	throw std::runtime_error("Unreachable");
+}
+
+
+std::pair<std::vector<uint8_t>, bool> node_t::get_downloaded_data(const CAN_Frame &frame) const{
+	const auto ccs = get_sdo_ccs(frame);
+	if(ccs != sdo_ccs_e::INIT_DOWNLOAD && ccs != sdo_ccs_e::SEGMENT_DOWNLOAD){
+		throw std::logic_error("Frame is not a download command: " + vnx::to_string_value(ccs));
+	}
+
+	if(ccs == sdo_ccs_e::INIT_DOWNLOAD){
+		return get_transferred_data_first_frame(frame);
+	}else if(ccs == sdo_ccs_e::SEGMENT_DOWNLOAD){
+		return get_transferred_data_next_frame(frame);
+	}
+
+	// -Wreturn-type
+	throw std::runtime_error("Unreachable");
+}
+
+
+std::shared_ptr<const CAN_Frame> node_t::abort_server(const uint16_t &index, const uint8_t &subindex, const sdo_error_e &error) const{
+	auto frame = abort_transfer(index, subindex, error);
+	frame->id = tx_sdo;
+	set_scs(*frame, sdo_scs_e::ABORT);
+	return frame;
+}
+
+
+std::shared_ptr<const CAN_Frame> node_t::abort_client(const uint16_t &index, const uint8_t &subindex, const sdo_error_e &error) const{
+	auto frame = abort_transfer(index, subindex, error);
+	frame->id = rx_sdo;
+	set_ccs(*frame, sdo_ccs_e::ABORT);
+	return frame;
 }
 
 
